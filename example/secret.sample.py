6734c492bfc8656fbb8ebb64ce89a656ebbd2bff
Azrood <53349565+Azrood@users.noreply.github.com>
2020-06-08 14:26:14 +0100

    create secret_sample

--- example/secret.sample.py
+++
@@ -1,4 +0,0 @@
-mail = "your email@provider.com"
-password = "your email password"
-csv_path = "path/to/csv/database"
-mailtext_path = "path/to/generic/mail/text"
---
+++ example/secret_sample.py
@@ -0,0 +1,6 @@
+user_mail = "your email@provider.com"
+password = "your email password"
+csv_path = "path/to/csv/database"
+mailtext_path = "path/to/generic/mail/text"
+smtp = "smtp.company.com"
+port = 465
